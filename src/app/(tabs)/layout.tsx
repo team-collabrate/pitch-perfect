b50c678a733819686d35ecd147aa52357e71ef27
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-06-07 12:55:28 +0530

    fix padding

--- src/app/(tabs)/layout.tsx
+++ src/app/(tabs)/layout.tsx
@@ -7,7 +7,7 @@ import { BottomNav } from "~/components/bottom-nav";
 export default function TabsLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-full flex-col">
-      <main className="flex-1 overflow-y-auto pt-2 pb-24">{children}</main>
+      <main className="flex-1 overflow-y-auto p-4 pt-2 pb-24">{children}</main>
       <BottomNav />
     </div>
   );
